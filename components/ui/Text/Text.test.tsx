16c0b504ed4aa010ecbc541cfc9b33d7210ddbf5
Hafidz <hafidzdev@gmail.com>
2021-11-29 10:56:18 +0700

    Text Component Unit Testing

--- components/ui/Text/Text.test.tsx
+++ components/ui/Text/Text.test.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Text from './Text';
 
-const MockTextCompoenent = () => {
-  return <Text>Assalamualaikum World</Text>;
-};
-
-describe('Text Componenent', () => {
+describe('Text Component', () => {
   test('Should be render Text Element', () => {
-    render(<MockTextCompoenent />);
-    const textComponent = screen.getByText('Assalamualaikum World');
+    render(<Text>Hello World</Text>);
+    const textComponent = screen.getByText('Hello World');
     expect(textComponent).toBeInTheDocument();
+  });
+
+  test('Tag Should be H1', () => {
+    render(
+      <Text data-testid='textComponent' as='h1'>
+        Hello World
+      </Text>,
+    );
+    const textComponent = screen.getByTestId('textComponent');
+    expect(textComponent.tagName).toBe('H1');
   });
 });
