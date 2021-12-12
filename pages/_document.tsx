7fad8e2d475721f6baf4102cde1829fa1c684bc8
Hafidz <hafidzdev@gmail.com>
2021-12-12 15:11:54 +0700

    change meta url

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,11 +16,11 @@ class MyDocument extends Document {
           <meta name='title' content='@hafidzamr' />
           <meta name='description' content='About Hafidz Amrulloh' />
           <meta property='og:type' content='website' />
-          <meta property='og:url' content='https://hafidzamr.dev/' />
+          <meta property='og:url' content='https://hafidzamr.tech/' />
           <meta property='og:title' content='Hafidz Amrulloh' />
           <meta property='og:description' content='About Hafidz Amrulloh' />
           <meta property='twitter:card' content='summary_large_image' />
-          <meta property='twitter:url' content='https://hafidzamr.dev/' />
+          <meta property='twitter:url' content='https://hafidzamr.tech/' />
           <meta property='twitter:title' content='About Hafidz Amrulloh' />
           <meta property='twitter:description' content='About Hafidz Amrulloh' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
