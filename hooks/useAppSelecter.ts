7c6c315cca8fdf9ef7aad1bb111e9cbc6efca82a
Hafidz <hafidzdev@gmail.com>
2021-12-12 12:28:05 +0700

    remove unnecessary lib and func

--- hooks/useAppSelecter.ts
+++
@@ -1,5 +0,0 @@
-import { useSelector, TypedUseSelectorHook } from 'react-redux';
-import { RootState } from '@/store';
-
-const useAppDispatch: TypedUseSelectorHook<RootState> = useSelector;
-export default useAppDispatch;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,11 @@
 import type { AppProps } from 'next/app';
-import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '@/theme.config';
 
-import { store } from '@/store';
-
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Provider store={store}>
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  </Provider>
+  <ChakraProvider resetCSS theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
 );
 
 export default MyApp;
