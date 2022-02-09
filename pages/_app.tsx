a164b30bb26280fcb3445f2750e0420eb85c81af
Hafidz <hafidzdev@gmail.com>
2022-02-09 20:45:58 +0700

    change id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,9 +15,10 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       router.events.off('routeChangeComplete', pageview);
     };
   }, [router.events]);
+
   return (
     <>
-      <Script id='google-tag-manager' strategy='afterInteractive'>
+      <Script id='gtm' strategy='afterInteractive'>
         {`
           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
