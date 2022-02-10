308dd1b2906c05a27a341c8fb0a5f3068ef45603
Hafidz <hafidzdev@gmail.com>
2022-02-10 19:25:18 +0700

    move gtm to env

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-P4S3P93');
+          })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');
       `}
       </Script>
 
