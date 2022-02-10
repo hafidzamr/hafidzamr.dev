80a963aa92d88f0337be195987afb34581b38bb6
Hafidz <hafidzdev@gmail.com>
2022-02-10 19:24:29 +0700

    add sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+  siteUrl: `${process.env.NEXT_PUBLIC_SITE_URL}`,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+    additionalSitemaps: [`${process.env.NEXT_PUBLIC_SITE_URL}/sitemap.xml`],
+  },
+};
---
+++ pages/server-sitemap.xml/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { GetServerSideProps } from 'next';
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
+
+import { allBlogs } from '.contentlayer/data';
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const sitemap: ISitemapField[] = allBlogs.map((blog) => ({
+    loc: `/blog/${blog.slug}`,
+    lastmod: new Date().toISOString(),
+  }));
+
+  return getServerSideSitemap(context, sitemap);
+};
+
+const SiteMap = () => {};
+
+export default SiteMap;
